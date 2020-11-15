969272b3e958bcf39aac724742d809c9fb01628b
NP Viroonc <npviro@gmail.com>
2020-11-14 16:56:58 -0800

    Update Linux Mint Setup Script.sh

--- Linux Mint Setup Script.sh
+++ Linux Mint Setup Script.sh
@@ -4,12 +4,14 @@ mainmenu () {
 	clear
  	tput setaf 3
 	echo "====================================="
-	echo " --- Linux Mint Setup Script 3.6 ---"
+	echo " --- Linux Mint Setup Script 3.7 ---"
 	echo "====================================="
 	echo "Supported Linux Mint Versions: 20.x"
 	echo "Script may prompt you or ask you for your password once in a while. Please monitor your computer until the script is done."
-	tput setaf 3
+	echo "This script will show terminal output. This is normal."
 	echo "You can open this script in a text editor to see packages to be installed in detail."
+	tput setaf 10
+	echo "You are encouraged to modify this script for your own needs."
 	tput setaf 9
 	echo "System will automatically reboot after the script is run!!!"
 	echo "It is not recommended to run this script more than once!!!"
@@ -125,50 +127,6 @@ finish () {
 	clear
 	sudo reboot
 }
-fullcommon () {
-	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
-	sudo dpkg-reconfigure libdvd-pkg
-	sudo dpkg --add-architecture i386
-	sudo apt update -y
-	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
-	sudo add-apt-repository -y ppa:linuxuprising/java
-	sudo apt update -y
-	sudo apt install -y oracle-java14-installer
-	java --version
-	sleep 3
-	java -version
-	sleep 3
-	sudo add-apt-repository -y ppa:mkusb/ppa
-	sudo apt update -y
-	sudo apt install -y mkusb mkusb-nox usb-pack-efi gparted
-	sudo add-apt-repository -y ppa:obsproject/obs-studio
-	sudo apt update -y
-	sudo apt install -y obs-studio
-	sudo add-apt-repository -y ppa:minetestdevs/stable
-	sudo apt update -y
-	sudo apt install -y minetest
-	curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
-	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-	sudo apt update -y
-	sudo apt install -y spotify-client
-	sudo apt update -y
-	sudo apt full-upgrade -y
-	sudo apt autoremove -y
-	sudo apt autoclean -y
-	finish
-}
-minimalcommon () {
-	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
-	sudo dpkg-reconfigure libdvd-pkg
-	sudo dpkg --add-architecture i386
-	sudo apt update -y
-	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
-	sudo apt update -y
-	sudo apt full-upgrade -y
-	sudo apt autoremove -y
-	sudo apt autoclean -y
-	finish
-}
 full () {
 	clear
 	tput setaf 3
@@ -177,8 +135,35 @@ full () {
 	sleep 3
 	clear
 	sudo apt update -y
-	sudo apt install -y ubuntu-restricted-extras synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox rhythmbox-plugin-alternative-toolbar shotwell solaar gparted vlc p7zip-full p7zip-rar lame gpart speedtest-cli neofetch ffmpeg httraqt lsp-plugins tree audacity telegram-desktop easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi supertux dconf-editor deja-dup gnome-todo pitivi fonts-cantarell gnome-books numlockx gnome-firmware gnome-weather krita gnome-clocks gimp htop nemo-image-converter nemo-media-columns nemo-audio-tab nemo-seahorse transmission curl git handbrake
-	fullcommon
+	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+	sudo dpkg-reconfigure libdvd-pkg
+	sudo apt install -y ubuntu-restricted-extras synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox shotwell solaar gnome-boxes gparted vlc p7zip-full p7zip-rar lame gpart speedtest-cli grub2-common neofetch network-manager-openvpn-gnome ffmpeg httraqt lsp-plugins tree audacity telegram-desktop gufw easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi supertux dconf-editor deja-dup gnome-todo pitivi ffmpegthumbs fonts-cantarell gnome-books krita gnome-clocks gimp htop transmission curl git handbrake gdebi gnome-weather gnome-firmware gucharmap gtk-3-examples youtube-dl numlockx nemo-image-converter nemo-media-columns nemo-audio-tab nemo-seahorse
+	sudo dpkg --add-architecture i386
+	sudo apt update -y
+	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
+	sudo add-apt-repository -y ppa:linuxuprising/java
+	sudo apt install -y oracle-java15-installer
+	java --version
+	sleep 3
+	java -version
+	sleep 3
+	sudo add-apt-repository -y ppa:mkusb/ppa
+	sudo apt install -y mkusb mkusb-nox usb-pack-efi gparted
+	sudo add-apt-repository -y ppa:obsproject/obs-studio
+	sudo apt install -y obs-studio
+	sudo add-apt-repository -y ppa:minetestdevs/stable
+	sudo apt install -y minetest
+	curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+	sudo apt update -y
+	sudo apt install -y spotify-client
+	sudo apt update -y
+	sudo apt full-upgrade -y
+	sudo apt autoremove -y --purge
+	sudo apt autoclean -y
+	echo "Adding current user to cdrom group..."
+	sudo usermod -aG cdrom $USER
+	finish
 }
 minimal () {
 	clear
@@ -188,8 +173,17 @@ minimal () {
 	sleep 3
 	clear
 	sudo apt update -y
-	sudo apt install -y ubuntu-restricted-extras synaptic pavucontrol rhythmbox rhythmbox-plugin-alternative-toolbar gparted p7zip-full p7zip-rar gpart network-manager-openvpn-gnome ffmpeg gufw dconf-editor deja-dup fonts-cantarell numlockx gnome-firmware htop nemo-image-converter nemo-media-columns nemo-audio-tab nemo-seahorse curl git
-	minimalcommon
+	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+	sudo dpkg-reconfigure libdvd-pkg
+	sudo apt install -y ubuntu-restricted-extras synaptic pavucontrol rhythmbox gparted p7zip-full p7zip-rar gpart network-manager-openvpn-gnome ffmpeg gufw dconf-editor deja-dup fonts-cantarell htop curl git gdebi gnome-firmware gucharmap gtk-3-examples numlockx nemo-image-converter nemo-media-columns nemo-audio-tab nemo-seahorse
+	sudo dpkg --add-architecture i386
+	sudo apt update -y
+	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
+	sudo apt update -y
+	sudo apt full-upgrade -y
+	sudo apt autoremove -y --purge
+	sudo apt autoclean -y
+	finish
 }
 fullalt () {
 	clear
@@ -199,19 +193,55 @@ fullalt () {
 	sleep 3
 	clear
 	sudo apt update -y
-	sudo apt install -y ubuntu-restricted-extras synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox rhythmbox-plugin-alternative-toolbar shotwell solaar gparted vlc p7zip-full p7zip-rar lame gpart speedtest-cli neofetch ffmpeg httraqt lsp-plugins tree audacity telegram-desktop easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi supertux dconf-editor deja-dup gnome-todo pitivi fonts-cantarell gnome-books numlockx gnome-firmware gnome-weather krita gnome-clocks gimp htop transmission curl git handbrake
-	fullcommon
+	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+	sudo dpkg-reconfigure libdvd-pkg
+	sudo apt install -y ubuntu-restricted-extras synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox shotwell solaar gnome-boxes gparted vlc p7zip-full p7zip-rar lame gpart speedtest-cli grub2-common neofetch network-manager-openvpn-gnome ffmpeg httraqt lsp-plugins tree audacity telegram-desktop gufw easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi supertux dconf-editor deja-dup gnome-todo pitivi ffmpegthumbs fonts-cantarell gnome-books krita gnome-clocks gimp htop transmission curl git handbrake gdebi gnome-weather gnome-firmware gucharmap gtk-3-examples youtube-dl
+	sudo dpkg --add-architecture i386
+	sudo apt update -y
+	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
+	sudo add-apt-repository -y ppa:linuxuprising/java
+	sudo apt install -y oracle-java15-installer
+	java --version
+	sleep 3
+	java -version
+	sleep 3
+	sudo add-apt-repository -y ppa:mkusb/ppa
+	sudo apt install -y mkusb mkusb-nox usb-pack-efi gparted
+	sudo add-apt-repository -y ppa:obsproject/obs-studio
+	sudo apt install -y obs-studio
+	sudo add-apt-repository -y ppa:minetestdevs/stable
+	sudo apt install -y minetest
+	curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+	sudo apt update -y
+	sudo apt install -y spotify-client
+	sudo apt update -y
+	sudo apt full-upgrade -y
+	sudo apt autoremove -y --purge
+	sudo apt autoclean -y
+	echo "Adding current user to cdrom group..."
+	sudo usermod -aG cdrom $USER
+	finish
 }
 minimalalt () {
 	clear
 	tput setaf 3
-	echo "Minimal Install/Essentials (Other DEs)..."
+	echo "Minimal Install/Essentials (Cinnamon)..."
 	tput sgr0
 	sleep 3
 	clear
 	sudo apt update -y
-	sudo apt install -y ubuntu-restricted-extras synaptic pavucontrol rhythmbox rhythmbox-plugin-alternative-toolbar gparted p7zip-full p7zip-rar gpart network-manager-openvpn-gnome ffmpeg gufw dconf-editor deja-dup fonts-cantarell numlockx gnome-firmware htop curl git
-	minimalcommon
+	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+	sudo dpkg-reconfigure libdvd-pkg
+	sudo apt install -y ubuntu-restricted-extras synaptic pavucontrol rhythmbox gparted p7zip-full p7zip-rar gpart network-manager-openvpn-gnome ffmpeg gufw dconf-editor deja-dup fonts-cantarell htop curl git gdebi gnome-firmware gucharmap gtk-3-examples
+	sudo dpkg --add-architecture i386
+	sudo apt update -y
+	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
+	sudo apt update -y
+	sudo apt full-upgrade -y
+	sudo apt autoremove -y --purge
+	sudo apt autoclean -y
+	finish
 }
 # End of Function Cluster
 # Start of Main Script
